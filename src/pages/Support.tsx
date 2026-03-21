463bc030c0d92ea6f97987dc22f3bb5562dd86af
buccijr <antoniobuccijr@gmail.com>
2026-03-21 09:19:15 -0400

    update

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -13,19 +13,28 @@ const section = [
     [
     {
    title: "Creating Stories",
-   content: 'To create a story in Slideory:\nEach story option can be either text or an image.\nBasic Editor: Allows you to create stories with linear options. Selecting an option will lead to a specific next slide (e.g., Slide 1A or Slide 2).\nAdvanced Editor: Enables branching stories, where each option can lead to any slide in the story, allowing for multiple possible paths and outcomes.'
+   content: 'To create a story in Slideory:\n\nEach story option can be either text or an image.\n\nBasic Editor: Allows you to create stories with linear options. Selecting an option will lead to a specific next slide (e.g., Slide 1A or Slide 2).\n\nAdvanced Editor: Enables branching stories, where each option can lead to any slide in the story, allowing for multiple possible paths and outcomes.'
+    },
+     {
+   title: "Publishing your story",
+   content: "When you're done with your story, click on the \"Finish\" button. This will take you to the publish page. There, you can add a cover image, up to five hashtags, and a cover image. You can also enable the lives feature for your story. To preview your story, click on the eye icon. To save a draft, click on the text document button."
     },
     {
    title: "Playing Stories",
-   content: 'Click on a story to start it. If you are on a option slide, as indicated by there being options, on the slide, you must select an option to continue. If not, swipe to continue.'
+   content: 'Click on a story to start it. If you are on a option slide, as indicated by there being options on the slide, you must select an option to continue. If not, swipe to continue.'
+    },
+     {
+   title: "Personalizing your profile",
+   content: 'Click on the edit button to customize your profile. There, you can edit your profile picture and banner. You can also add a bio! To change the color scheme of your profile, select a color under the "theme" section.'
     },
 ]
-  }
+  },
+ 
 ];
 return (
     <div className=" min-h-screen p-20 flex flex-col bg-gradient-to-b from-pink-200 to-white">
   <h1 className=" font-poppins font-bold text-6xl text-black">Support</h1>
-  <p className="mt-6 font-poppins font-black text-2xl text-[rgb(229,162,255)]">How can we help you?
+  <p className="mt-6 font-poppins font-black text-2xl text-[rgb(220,133,255)]">How can we help you?
 
 </p>
                                                {section.map((e: any) => {
@@ -33,16 +42,16 @@ return (
                                                {                 
                                              
                                                <div>                         
-                                                   <p className="font-poppins text-black text-3xl font-bold">{e.title}</p>
+                                                   <p className="font-poppins text-black text-4xl font-bold">{e.title}</p>
                                                 {e.title =='Contact'  &&  <p className="font-poppins text-black text-lg mt-5">{e.content}</p>}
                                                 
                                                  { e.title  != 'Contact' &&
                                                             e.content.map((e: any) => {
-                                            return   <div className="flex flex-col mt-5">
+                                            return   <div className="flex flex-col mt-8">
                                                         
                                                                         
                                                    <p className="font-poppins text-black text-2xl font-bold">{e.title}</p>
-                                                <p className="font-poppins text-black text-lg mt-5">{e.content}</p>
+                                                <p className="font-poppins text-black text-lg mt-5 whitespace-pre-line">{e.content}</p>
                                                 </div>
                                                
                                                             })
